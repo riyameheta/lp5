b03dc7cff3b37e77e4abd447fc918e1e93e26150
riyameheta <168670829+riyameheta@users.noreply.github.com>
2025-05-04 20:52:49 +0530

    Update bubble.cpp

--- bubble.cpp
+++ bubble.cpp
@@ -75,3 +75,87 @@ int main()
     
     return 0;
 }
+
+
+// #include <iostream>
+// #include <vector>
+// #include <omp.h>
+// #include <chrono>
+
+// using namespace std;
+
+// void sequentialBubbleSort(vector<int>& arr) {
+//     int n = arr.size();
+//     for (int i = 0; i < n - 1; ++i)
+//         for (int j = 0; j < n - i - 1; ++j)
+//             if (arr[j] > arr[j + 1])
+//                 swap(arr[j], arr[j + 1]);
+// }
+
+// // Parallel Odd-Even Bubble Sort
+// void parallelBubbleSortOddEven(vector<int>& arr) {
+//     int n = arr.size();
+//     for (int i = 0; i < n; ++i) {
+//         if (i % 2 == 0) {
+//             #pragma omp parallel for
+//             for (int j = 0; j < n - 1; j += 2)
+//                 if (arr[j] > arr[j + 1])
+//                     swap(arr[j], arr[j + 1]);
+//         } else {
+//             #pragma omp parallel for
+//             for (int j = 1; j < n - 1; j += 2)
+//                 if (arr[j] > arr[j + 1])
+//                     swap(arr[j], arr[j + 1]);
+//         }
+//     }
+// }
+
+// void printArray(const vector<int>& arr) {
+//     for (int val : arr)
+//         cout << val << " ";
+//     cout << "\n";
+// }
+
+// int main() {
+//     int n;
+//     cout << "Enter number of elements: ";
+//     cin >> n;
+
+//     if (n <= 0) {
+//         cout << "Invalid size.\n";
+//         return 1;
+//     }
+
+//     vector<int> original(n);
+//     cout << "Enter elements:\n";
+//     for (int i = 0; i < n; ++i) {
+//         cout << "Element " << i + 1 << ": ";
+//         cin >> original[i];
+//     }
+
+//     vector<int> seqArr = original;
+//     vector<int> parArr = original;
+
+//     // Sequential sort timing
+//     auto startSeq = chrono::high_resolution_clock::now();
+//     sequentialBubbleSort(seqArr);
+//     auto endSeq = chrono::high_resolution_clock::now();
+
+//     // Parallel sort timing
+//     auto startPar = chrono::high_resolution_clock::now();
+//     parallelBubbleSortOddEven(parArr);
+//     auto endPar = chrono::high_resolution_clock::now();
+
+//     chrono::duration<double, milli> timeSeq = endSeq - startSeq;
+//     chrono::duration<double, milli> timePar = endPar - startPar;
+
+//     cout << "\n=== Sequential Bubble Sort ===\n";
+//     printArray(seqArr);
+//     cout << "Time Taken: " << timeSeq.count() << " ms\n";
+
+//     cout << "\n=== Parallel Odd-Even Bubble Sort ===\n";
+//     printArray(parArr);
+//     cout << "Time Taken: " << timePar.count() << " ms\n";
+
+//     return 0;
+// }
